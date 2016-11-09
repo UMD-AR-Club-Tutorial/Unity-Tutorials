e2328dec33f94c85eb140ec1543c9417193da562
Brian Servia <Brian Servia>
2016-11-09 16:13:00 -0500

    exercise1

---
+++ Scripts/Follow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject player;
+    private Vector3 offset;
+    void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ Scripts/agent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class agent : MonoBehaviour {
+    NavMeshAgent agentt;
+	// Use this for initialization
+	void Start () {
+        agentt = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 100))
+            {
+                agentt.SetDestination(hit.point);
+            }
+        }
+	}
+}
